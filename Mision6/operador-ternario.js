f180c7bcf7e8ac4f93fec34158774de263fd55d2
Nancy Escobar <nancescq@gmail.com>
2023-06-16 17:06:20 -0500

    Actualizando Mision 6 16Jun

---
+++ Mision6/operador-ternario.js
@@ -0,0 +1,4 @@
+let num = 8;
+
+num === 8 ? console.log ( "El Numero " + num + " es igual a 8" ) : console.log ( "El Numero " + num + " NO es igual a 8" );
+
---
+++ Mision6/saludando.js
@@ -0,0 +1,9 @@
+let time = 20;
+
+if (time < 12){
+    console.log ( "Buenos Días");
+} else if ( time >= 12 && time <= 18) {
+    console.log ( "Buenos Tardes");
+} else {
+    console.log ( "Buenos Noches");
+}
